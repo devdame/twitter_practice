c40bad323f354ddf625b288b0150fbf9771caaa7
thejessleigh <j.l.unrein@gmail.com>
2014-03-03 13:58:07 -0600

    added updated at field to tweet to help manage the cache.

--- db/migrate/20140303133348_create_tweets.rb
+++ db/migrate/20140303133348_create_tweets.rb
@@ -4,6 +4,8 @@ class CreateTweets < ActiveRecord::Migration
       t.string :text
       t.integer :twitter_user_id
       t.timestamp :created_at
+
+      t.timestamp :updated_at
     end
   end
 end
