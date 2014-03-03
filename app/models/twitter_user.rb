fe8f29b9f5d449f6e1b4696b4f466dc6f275c459
thejessleigh <j.l.unrein@gmail.com>
2014-03-03 14:27:20 -0600

    tweets_stale? now checks against user's average tweet time, rather than a static number.

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -8,7 +8,18 @@ class TwitterUser < ActiveRecord::Base
   end
 
   def tweets_stale?
-    Time.now.utc - self.tweets.first.updated_at > 900
+    Time.now.utc - self.tweets.first.updated_at > average_tweet_time
+  end
+
+  def average_tweet_time
+    tweet_diffs = 0
+    all_tweets = self.tweets
+    all_tweets.each_with_index do |tweet, index|
+      unless index == all_tweets.length - 1
+        tweet_diffs += (tweet.created_at - all_tweets[index + 1].created_at).abs
+      end
+    end
+    tweet_diffs/all_tweets.length
   end
 
 end
